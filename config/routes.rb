cfb5ce2547cc0b9926cb10005235f69835a817f4
Akihiro <kouen.xyz1@gmail.com>
2019-02-14 22:06:34 +0000

    create files related with budget directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
+  get "home/index" => "home#index"
+  get "home/show/:id" => "home#show"
+  get "household" => "household#index"
+  get "budget" => "budget#index"
 end
