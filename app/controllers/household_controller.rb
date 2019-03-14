fe26fbb9aca2c276aeecbef45e253eb44ce686ac
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-03-14 21:47:06 +0900

    householdのinsert処理

--- app/controllers/household_controller.rb
+++ app/controllers/household_controller.rb
@@ -3,4 +3,26 @@ class HouseholdController < ApplicationController
   def index
     @households = Household.where(user_id: current_user.id)
   end
+  
+  def new
+  end
+  
+  def create
+    Household.create(
+      user_id: current_user.id,
+      entry_date: params[:entry_date],
+      category: params[:category],
+      sub_category1: params[:sub_category1],
+      sub_category2: params[:sub_category2],
+      payment: params[:payment],
+      result: params[:result],
+      sum_credit: params[:sum_credit],
+      check_out: params[:check_out],
+      detail: params[:detail],
+      analysis: params[:analysis],
+      client: params[:client],
+      content: params[:content]
+      )
+    redirect_to action: :index and return
+  end
 end
