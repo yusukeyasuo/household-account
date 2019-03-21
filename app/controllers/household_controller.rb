94bb1dd99c23e8419102fe39a5b58ee6171df5eb
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2019-03-21 14:44:55 +0900

    householdの更新処理

--- app/controllers/household_controller.rb
+++ app/controllers/household_controller.rb
@@ -1,28 +1,37 @@
 class HouseholdController < ApplicationController
   before_action :require_login
+  before_action :set_household, only: [:edit, :update]
+  
   def index
     @households = Household.where(user_id: current_user.id)
   end
   
   def new
+    @household = Household.new
   end
   
   def create
-    Household.create(
-      user_id: current_user.id,
-      entry_date: params[:entry_date],
-      category: params[:category],
-      sub_category1: params[:sub_category1],
-      sub_category2: params[:sub_category2],
-      payment: params[:payment],
-      result: params[:result],
-      sum_credit: params[:sum_credit],
-      check_out: params[:check_out],
-      detail: params[:detail],
-      analysis: params[:analysis],
-      client: params[:client],
-      content: params[:content]
-      )
+    household = Household.new(household_params)
+    household.user_id = current_user.id
+    household.save
     redirect_to action: :index and return
   end
+  
+  def edit
+  end
+  
+  def update
+    @household.update(household_params)
+    redirect_to action: :index and return
+  end
+  
+  private
+    def set_household
+      @household = Household.find_by(id: params[:id])
+      redirect_to :household_index_path and return unless @household
+    end
+    
+    def household_params
+      params.require(:household).permit(:user_id, :entry_date, :category, :sub_category1, :sub_category2, :payment, :result, :sum_credit, :check_out, :detail, :analysis, :client, :content)
+    end
 end
